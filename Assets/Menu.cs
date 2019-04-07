6b9246f87cd0923d6dc6520ab26063e79aaea3db
sohe1l <soheilansari@icloud.com>
2019-04-06 19:27:50 -0700

    Added main scenes

---
+++ Assets/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Scene transition numbers will be finalised later. It's hard coded for convenience. 
+// Can see scene numbers in File > build settings
+public class Menu : MonoBehaviour
+{
+    public void PlayTraining()
+    {
+        SceneManager.LoadScene("Training");
+    }
+    public void PlayScored()
+    {
+        SceneManager.LoadScene("Scored");
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+    public void EndScreen()
+    {
+        SceneManager.LoadScene("End");
+    }
+}
