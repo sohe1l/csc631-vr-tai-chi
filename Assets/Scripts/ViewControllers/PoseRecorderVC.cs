c144390b96f044a5b0f81dcdc851dd5ab7ce6de7
Soheil <soheilansari@icloud.com>
2019-04-10 14:44:17 -0700

    Updated Pose Recorder

--- Assets/Scripts/ViewControllers/PoseRecorderVC.cs
+++ Assets/Scripts/ViewControllers/PoseRecorderVC.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using Valve.VR;
 
 public class PoseRecorderVC : MonoBehaviour
 {
@@ -28,12 +29,14 @@ public class PoseRecorderVC : MonoBehaviour
     void Start()
     {
         // Activate VR
-        StartCoroutine(SetVRDevice("OpenVR", true));
+        //StartCoroutine(SetVRDevice("OpenVR", true));
+
     }
 
     void Update()
     {
-        if (Input.GetKey(TRIGGER))
+            
+        if (SteamVR_Input.GetBooleanAction("GrabPinch").state)
         {
             Vector3 right = InputTracking.GetLocalPosition(XRNode.RightHand);
             Vector3 left = InputTracking.GetLocalPosition(XRNode.LeftHand);
@@ -53,6 +56,15 @@ public class PoseRecorderVC : MonoBehaviour
                 if (last_elapsed != elapsed && count != 0)
                 {
                     dict.Add(elapsed, new double[] { x_sum / count, y_sum / count, z_sum / count });
+
+                    if(dict.Count != 0)
+                    {
+
+                        double[] start = dict[elapsed - 1];
+                        DrawLine(new Vector3((float)start[0], (float)start[1], (float)start[2]), new Vector3((float)x_sum / count, (float)y_sum / count, (float)z_sum / count), Color.blue);
+                    }
+
+
                     count = 0;
                     x_sum = 0;
                     y_sum = 0;
@@ -130,6 +142,25 @@ public class PoseRecorderVC : MonoBehaviour
         XRSettings.enabled = vrEnabled;
     }
 
+
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        // lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+
+        lr.startColor = color;
+
+        lr.startWidth = 0.1f;
+        lr.startWidth = 0.01f;
+
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        //GameObject.Destroy(myLine, duration);
+    }
 
 }
 
