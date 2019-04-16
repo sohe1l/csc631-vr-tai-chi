827fab6f07c35a2030690ead7ad57eb9e2310462
sohe1l <soheilansari@icloud.com>
2019-04-16 13:25:31 -0700

    Imported Fast IK

---
+++ Assets/FastIK/Scripts/Sample/SampleRotator.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+namespace DitzelGames.FastIK
+{
+    public class SampleRotator : MonoBehaviour
+    {
+        
+        void Update()
+        {
+            //just rotate the object
+            transform.Rotate(0, Time.deltaTime * 90, 0);
+        }
+    }
+}
