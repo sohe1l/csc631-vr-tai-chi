d7b09f3b692594ad6c73ff696b8705afde926b99
Soheil <soheilansari@icloud.com>
2019-04-16 11:35:37 -0700

    Pose recorder removed hands

--- Assets/Scripts/Models/TimePoint.cs
+++ Assets/Scripts/Models/TimePoint.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 
 using SQLite4Unity3d;
 
-public class TimePoint {
+public class TimePoint
+{
     public const int TYPE_HAND_LEFT = 0;
     public const int TYPE_HAND_RIGHT = 1;
     public const int TYPE_FOOT_LEFT = 2;
     public const int TYPE_FOOT_RIGHT = 3;
     public const int TYPE_HEAD = 4;
     public const int TYPE_WAIST = 5;
-     [PrimaryKey, AutoIncrement]     public int Id { get; set; }
+
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
 
     public int PoseID { get; set; }
 
     public int Type { get; set; }
-     public double X { get; set; }     public double Y { get; set; }     public double Z { get; set; }      public int Time { get; set; }
-} 
+
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public int Time { get; set; }
+}
