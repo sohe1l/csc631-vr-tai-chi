8b0a32f050548dac7558b104d7926b59c1f01cc0
Martin Lee <33700647+Marrlee@users.noreply.github.com>
2019-04-19 14:14:14 -0700

    Functions for UI
    Started functions for UI

--- Assets/Scripts/ViewControllers/GameVC.cs
+++ Assets/Scripts/ViewControllers/GameVC.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class GameVC : MonoBehaviour
 {
+
+    private int currentScore;
+    private int Level;
+
     // Start is called before the first frame update
     void Start()
     {
+        Level = Prefs.GetLevelID();
+
         // Debug.Log(Prefs.GetLevelID());
         // Debug.Log(Prefs.GetPlayerName());
+
+        
 
     }
 
@@ -16,5 +24,57 @@ public class GameVC : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    void loadLevel()
+    {
+
+    }
+
+    void updatePose()
+    {
+
+    }
+
+
+    void showNirvana()
+    {
+
+    }
+
+    void showMoveInRange()
+    {
+
+    }
+
+    void showMoveOffRange()
+    {
+
+    }
+
+    // update chi meter for nirvana during the game
+    void updateChiMeter(int score)
+    {
+
+    }
+
+
+
+    // update current score in during game play
+    void updateScore()
+    {
+
+    }
+
+    // saves score to database for leaderboard
+    void saveScoreToLeaderboard()
+    {
+
+    }
+
+    void endGame()
+    {
+
     }
 }
