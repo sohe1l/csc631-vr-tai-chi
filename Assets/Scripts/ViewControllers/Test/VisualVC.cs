60aa47031a4eaa2f9623c521102134afbbf4292a
sohe1l <soheilansari@icloud.com>
2019-04-24 19:21:42 -0700

    Visual guides implemented.
    Visual guides between hand controller and desired point on path for each data point implemented. Initial pose works upon play but pose button functionality is currently broken.

---
+++ Assets/Scripts/ViewControllers/Test/VisualVC.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SQLite4Unity3d;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.XR;
+
+public class VisualVC : MonoBehaviour
+{
+    public Transform[] FootTarget;
+    public Transform LookTarget;
+    public Transform HandTarget;
+    public Transform HandPole;
+    public Transform Step;
+    public Transform Attraction;
+
+    public Camera camera;
+
+
+
+    public Transform TargetHandLeft;
+    public Transform TargetHandRight;
+
+    public GameObject RealHandLeft;
+    public GameObject RealHandRight;
+    public GameObject RealHead;
+    public GameObject Master;
+
+
+    int counter = 0;
+    TableQuery<TimePoint> QueryLeft;
+    IEnumerator<TimePoint> EQ_Left;
+    TableQuery<TimePoint> QueryRight;
+    IEnumerator<TimePoint> EQ_Right;
+
+    float delta = 0;
+
+    public void SwitchPose1()
+    {
+        SwitchPose(1);
+    }
+
+    public void SwitchPose2()
+    {
+        SwitchPose(2);
+    }
+
+    public void SwitchPose3()
+    {
+        SwitchPose(3);
+    }
+
+    public void SwitchPose(int PoseID)
+    {
+        var db = DataService.Instance.GetConnection();
+
+        QueryLeft = db.Table<TimePoint>()
+            .Where(v => v.PoseID.Equals(PoseID))
+            .Where(v => v.Type.Equals(TimePoint.TYPE_HAND_LEFT));
+
+        QueryRight = db.Table<TimePoint>()
+            .Where(v => v.PoseID.Equals(PoseID))
+            .Where(v => v.Type.Equals(TimePoint.TYPE_HAND_RIGHT));
+
+        EQ_Left = QueryLeft.GetEnumerator();
+        EQ_Right = QueryRight.GetEnumerator();
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Utils.SetVRDevice("OpenVR", true));
+        SwitchPose1();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+
+            delta += Time.deltaTime;
+            if (delta > 0.1)
+            {
+                RealHandLeft.transform.position = UnityEngine.XR.InputTracking.GetLocalPosition(XRNode.LeftHand);
+                RealHandRight.transform.position = UnityEngine.XR.InputTracking.GetLocalPosition(XRNode.RightHand);
+
+
+                EQ_Left.MoveNext();
+                TimePoint tp = EQ_Left.Current;
+                TargetHandLeft.transform.position = new Vector3((float)tp.X, (float)tp.Y, (float)tp.Z);
+
+
+                EQ_Right.MoveNext();
+                TimePoint tpr = EQ_Right.Current;
+                TargetHandRight.transform.position = new Vector3((float)tpr.X, (float)tpr.Y, (float)tpr.Z);
+
+                //Debug.Log(RealHandLeft.transform.position);
+
+                //first input is database value, second input is player input value
+                //DrawVisual(TargetHandLeft.transform.position, RealHandLeft.transform.position);
+                //DrawVisual(RealHandLeft.transform.position, RealHandLeft.transform.position);
+
+                StartCoroutine(DrawVisual(TargetHandLeft.transform.position, RealHandLeft.transform.position));
+                StartCoroutine(DrawVisual(TargetHandRight.transform.position, RealHandRight.transform.position));
+
+                delta = 0;
+            }
+
+        }
+        catch
+        {
+
+        }
+    }
+
+    //first input is database value, second input is player input value
+    IEnumerator DrawVisual(Vector3 real, Vector3 trial)
+    {
+        bool acceptable = validatePoint(real, trial);
+
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = real;
+        sphere.transform.localScale = new Vector3((float).1, (float).1, (float).1);
+
+        Renderer objectRenderer = sphere.GetComponent<Renderer>();
+        objectRenderer.material.color = Color.green;
+
+        GameObject.Destroy(sphere, (float) 0.1);
+
+        DrawLine(real, trial, Color.red);
+
+        yield return null;
+    }
+
+
+    void DrawLine(Vector3 real, Vector3 trial, Color c1)
+    {
+        //Debug.Log("called from guideline @@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        GameObject myLine = new GameObject();
+        myLine.transform.position = real;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+
+        if (validatePoint(real, trial) == true)
+        {
+            c1 = Color.green;
+        }
+        else
+        {
+            c1 = Color.red;
+        }
+
+        float duration = (float) .1;
+
+        lr.material = new Material(Shader.Find("Sprites/Default"));
+        lr.SetColors(c1, c1);
+        lr.SetWidth(.02f, .02f);
+        lr.SetPosition(0, trial);
+        lr.SetPosition(1, real);
+        GameObject.Destroy(myLine, duration);
+    }
+
+
+    bool validatePoint(Vector3 positionTrue, Vector3 positionTrial, double acceptableDistance = .8)
+    {
+        //Debug.Log("distance between points: " + Vector3.Distance(positionTrue, positionTrial));
+        if (Vector3.Distance(positionTrue, positionTrial) <= acceptableDistance)
+        {
+            //Debug.Log("green");
+            return true;
+        }
+        else
+        {
+            //Debug.Log("red");
+            return false;
+        }
+    }
+}
