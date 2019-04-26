642bbc31384658ac2f763ffb542c15f4334fde24
Martin Lee <33700647+Marrlee@users.noreply.github.com>
2019-04-25 21:21:35 -0700

    Added Green border for in range, fonts for UI
    Fonts for UI, green border

--- Assets/Scripts/ViewControllers/GameVC.cs
+++ Assets/Scripts/ViewControllers/GameVC.cs
@@ -9,20 +9,22 @@ public class GameVC : MonoBehaviour
     private int currentScore;
     private int Level;
     public GameObject Player;
-    public GameObject redScreen;
+    public GameObject RedScreen;
     public GameObject YellowScreen;
+    public GameObject GreenScreen;
     public Text score;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //hardcoded score
+        currentScore = 4569;
         // Start VR
         StartCoroutine(Utils.SetVRDevice("OpenVR", true));
 
         Level = Prefs.GetLevelID();
-        currentScore = 31991;
+        
        
 
         // Debug.Log(Prefs.GetLevelID());
@@ -37,6 +39,7 @@ public class GameVC : MonoBehaviour
     {
         HideAllOverlays();
         showMoveOffRange();
+        showMoveInRange();
         showNirvana();
         updateScore();
 
@@ -57,14 +60,15 @@ public class GameVC : MonoBehaviour
 
     void HideAllOverlays()
     {
-        redScreen.SetActive(false);
+        RedScreen.SetActive(false);
         YellowScreen.SetActive(false);
+        GreenScreen.SetActive(false);
 
     }
 
 
     void showNirvana()
-    {
+    {   //shows yellow border if player is in nirvana state 
         if (Player.transform.position == new Vector3(1, 0, 0))
         {
             YellowScreen.SetActive(true);
@@ -72,15 +76,18 @@ public class GameVC : MonoBehaviour
     }
 
     void showMoveInRange()
-    {
-
+    {   //shows green border if player is in range of move 
+        if (Player.transform.position == new Vector3(0, 0, 1))
+        {
+            GreenScreen.SetActive(true);
+        }
     }
 
     void showMoveOffRange()
-    {
-        if(Player.transform.position == new Vector3(0,0,0))
+    {   //shows red border if player is out of range 
+        if (Player.transform.position == new Vector3(0,0,0))
         {
-            redScreen.SetActive(true);
+            RedScreen.SetActive(true);
         }
     }
 
@@ -95,6 +102,7 @@ public class GameVC : MonoBehaviour
     // update current score in during game play
     void updateScore()
     {
+       
         score.text = "Score: " + currentScore.ToString();
 
     }
