75a7e793e9e913a581bcc97219e326c468c50511
pancreaspinch <yobrianh@gmail.com>
2019-05-09 01:30:57 -0700

    added audio manager, changed music file structure

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        foreach(Sound s in sounds)
+        {
+           s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }   
+    }
+
+    void Start()
+    {
+        play("Forest");
+    }
+
+    // Update is called once per frame
+    public void play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("sound not found");
+            return;
+        }
+        s.source.Play();
+    }
+}
